17e64166060e67f6712e0c261e95921030b75fa4
JavaSampleApproach <javasampleapproach@gmail.com>
2018-12-14 08:30:16 +0000

    upadate commit

--- src/main/java/com/grokonez/restapi/SpringBootApIsApplication.java
+++ src/main/java/com/grokonez/restapi/SpringBootApIsApplication.java
@@ -1,14 +1,26 @@
 package com.grokonez.restapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBootApIsApplication {
+import com.grokonez.restapi.repository.CustomerRepository;
 
+@SpringBootApplication
+public class SpringBootApIsApplication implements CommandLineRunner{
+
+	@Autowired
+	CustomerRepository repository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApIsApplication.class, args);
 	}
-
+ 
+	@Override
+	public void run(String... arg0) throws Exception {
+		// clear all record if existed before do the tutorial with new data 
+		repository.deleteAll();
+	}
 }
 
